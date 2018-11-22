e35859863b10253f7ca51591b0206fc779080db2
Al <asanchez@nativeka.tk>
2018-11-22 17:17:25 -0600

    preparing for integration test

--- startup/db.js
+++ startup/db.js
@@ -1,7 +1,9 @@
 const winston = require('winston');
 const mongoose = require('mongoose');
+const config = require('config');
 
 module.exports = function() {
-  mongoose.connect('mongodb://localhost/vidly')
-    .then(() => winston.info('Connected to MongoDB...'));
+  const db = config.get('db');
+  mongoose.connect(config.get('db'))
+    .then(() => winston.info(`Connected to ${db}...`));
 }
