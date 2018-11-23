49b7098b3aeaf65c220a9bbf2d744e6cad51e4f7
Al <asanchez@nativeka.tk>
2018-11-23 07:16:35 -0600

    using isertMany to push data to mongodb with dummy genres then test it expecting the length to be 2

--- tests/integration/genres.test.js
+++ tests/integration/genres.test.js
@@ -9,8 +9,15 @@ describe('/api/genres', ()=>{
 
     describe('GET /', ()=>{
         it('should return all genres', async ()=>{
+            await Genre.collection.insertMany([
+                {name: 'genre1'},
+                {name: 'genre2'},
+            ])
+
             const res = await request(server).get('/api/genres');
+
             expect(res.status).toBe(200);
+            expect(res.body.length).toBe(2);
         });
     });
 });
