53380039086f8283e6c2debce7724cdea5ade3e8
Al <asanchez@nativeka.tk>
2018-11-23 07:48:39 -0600

    testing the app

--- tests/integration/genres.test.js
+++ tests/integration/genres.test.js
@@ -13,10 +13,12 @@ describe('/api/genres', ()=>{
 
     describe('GET /', ()=>{
         it('should return all genres', async ()=>{
-            await Genre.collection.insertMany([
+            const genres = [
                 {name: 'genre1'},
-                {name: 'genre2'}
-            ])
+                {name: 'genre2'},
+            ];
+
+            await Genre.collection.insertMany(genres);
 
             const res = await request(server).get('/api/genres');
 
@@ -24,10 +26,19 @@ describe('/api/genres', ()=>{
             expect(res.body.length).toBe(2);
             expect(res.body.some(g => g.name === 'genre1')).toBeTruthy();
             expect(res.body.some(g => g.name === 'genre2')).toBeTruthy();
-
         });
     });
 
 
-    describe('GET /:id', ()=>)
+    describe('GET /:id', ()=>{
+        it('should return a genre if valid id is passed', async ()=>{
+            const genre = new Genre({ name: 'genre1' });
+            await genre.save();
+
+            const res = await request(server).get('/api/genres/' + genre._id)
+
+            expect(res.status).toBe(200);
+            expect(res.body).toHaveProperty('name', genre.name);
+        });
+    });
 });
